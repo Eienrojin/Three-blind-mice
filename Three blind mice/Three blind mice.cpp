3dcf17577c38bffbd8aaf3943c137cb83017ab19
Nikita Polunin (二木立・半人) <Stkchanelx@gmail.com>
2021-10-16 19:28:06 +0300

    Добавьте файлы проекта.

---
+++ Three blind mice/Three blind mice.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+void mice();
+void see();
+
+int main()
+{
+    mice();
+    mice();
+        see();
+        see();
+}
+
+void mice()
+{
+    cout << "Three blind mice" << endl;
+}
+
+void see()
+{
+    cout << "See how they run" << endl;
+}
